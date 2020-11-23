f8b7f67e2be37ef58be33fab9dcc7830ff0638cd
anhlt <42168740+BryantShepherd@users.noreply.github.com>
2020-11-23 18:31:52 +0700

    feat: create new group message

--- src/components/messageScreen/CreateNewMessageDialog.vue
+++ src/components/messageScreen/CreateNewMessageDialog.vue
@@ -7,15 +7,28 @@
       </v-btn>
     </template>
     <v-card>
-      <user-search-box :options="[]" />
+      <v-autocomplete
+        v-model="selectedMembers"
+        :items="members"
+        solo
+        chips
+        label="Select"
+        item-text="name"
+        item-value="id"
+        multiple
+        deletable-chips
+        class="px-2 pt-1"
+      >
+      </v-autocomplete>
       <v-textarea
+        class="px-2"
         v-model="newMessage"
         solo
         flat
         name="message-text-box"
         placeholder="Send a message..."
         id="message-text-box-input"
-        rows="2"
+        rows="4"
         no-resize
         :background-color="isDark ? undefined : '#f5f5f5'"
       ></v-textarea>
@@ -34,27 +47,40 @@
 </template>
 
 <script>
-import UserSearchBox from "./UserSearchBox.vue";
+import { mapState } from "vuex";
 export default {
   data() {
     return {
       dialog: false,
       newMessage: "",
+      members: [],
+      selectedMembers: [],
     };
   },
-  components: { UserSearchBox },
   methods: {
     onAccept() {
       // submit message
+      this.$emit("submit", this.newMessage, this.selectedMembers);
+      this.dialog = false;
     },
     onCancel() {
       this.dialog = false;
+      this.newMessage = "";
+      this.selectedMembers = [];
     },
   },
   computed: {
     isDark() {
       return this.$vuetify.theme.dark;
     },
+    ...mapState({
+      currentClassroom: state => state.Classroom.currentClassroom,
+      currentUser: state => state.Auth.user,
+    }),
+  },
+  async mounted() {
+    let currentClassMembers = await this.$store.dispatch("FETCH_CLASS_MEMBERS", this.currentClassroom.id);
+    this.members = currentClassMembers.filter(m => m.id != this.currentUser.id);
   },
 };
 </script>
